d8b93d44766b7436d6b4df125b0a5c14a18b9a9c
marley1990 <b.marco_90@hotmail.it>
2013-03-06 13:46:16 +0100

    modifiche

--- 2013-03-04/exercise01.js
+++ 2013-03-04/exercise01.js
@@ -1,7 +1,7 @@
 for (var i = 1; i <= 10; i++) {
 	var valore="";
 	for (var j =1; j <= 10; j++){ 
-		valore+="i*j"+"  ";
+		valore+=i*j+" \t";
 	}
-	console.log(valore+"\t");
+	console.log(valore);
 }
